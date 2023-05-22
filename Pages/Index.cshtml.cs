30bfe884ef7da45eba1e9917241d71d38cefef8d
RNisha10 <133236491+RNisha10@users.noreply.github.com>
2023-05-22 16:31:54 +0200

    t22

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,22 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Runtime.InteropServices;
+using System.Security.Claims;
 
 namespace dotnetcoresample.Pages;
 
 public class IndexModel : PageModel
 {
 
-    public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
-    
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
+    public IEnumerable<Claim> UserClaims { get; set; }
     public void OnGet()
-    {        
+    {
+	    UserClaims = HttpContext.User.Claims;
     }
 }
