89b6399e04732eabe77595e9ed2fab376f5c3c44
RNisha10 <133236491+RNisha10@users.noreply.github.com>
2023-05-23 11:08:56 +0200

    t84

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -18,7 +18,7 @@ public class IndexModel : PageModel
     [JsonPropertyName("typ")]
     public string Type { get; set; }
     [JsonPropertyName("val")]
-    public string Value { get; set; }
+    public string Value { get { return Value; } }
 
     public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
     
